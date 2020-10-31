14f435b11c929c659d39839d7874a13abffa10cd
Elisa Septiani Lubis <70183669+elisaseptianilubis1009@users.noreply.github.com>
2020-10-31 14:27:55 +0700

    Update server.php

--- server.php
+++ server.php
@@ -7,7 +7,7 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-/*$uri = urldecode(
+$uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
@@ -19,37 +19,5 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 }
 
 require_once __DIR__.'/public/index.php';
-*/
 
-include "koneksi.php"; 
-$input1=$_GET['param1'];
-$input2=$_GET['param2'];
 
-$data=file_get_contents("php://input");
-	if((file_put_contents(rand().".jpg",$data))){
-	
-	echo "upload complete"." Data ".$input1." & ".$input2;
-
-	// $mysqli="INSERT INTO db_data (id,first_name,last_name,image) VALUES ('','$input1','$input2','$data')";
-	// mysqli_query($connect,$mysqli);
-
-	// $select="select * from db_data";
-	// $query=mysqli_query($connect,$select);
-	
-	// if(!$query){
-	// 		die ('SQL Error :'.mysqli_error($connect));
-	// }
-
-	// while ($row=mysqli_fetch_array($query)){
-
-	// 	echo $row['id'];
-	// 	echo $row['first_name'];
-	// 	echo $row['last_name'];
-	// 	echo $row['image'];
-	// }
-	// echo "Input Berhasil"; 
-}else{
-        echo "upload gagal"." Data ".$input1." & ".$input2;
-
-    }
-?>
